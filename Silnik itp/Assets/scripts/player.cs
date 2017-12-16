ebfcb683847b8270216746149c370b07e6b59c3c
kinop <paladyn.franek@gmail.com>
2017-12-16 15:54:49 +0100

    add basic idle bandit
    all by proffesssionnal tutorial

---
+++ Silnik itp/Assets/scripts/player.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class player : MonoBehaviour {
+	public float speed = 50f;
+	public float jump_power = 150f;
+	public bool grounded = false;
+	private Rigidbody2D rb2D;
+
+
+	// Use this for initialization
+	void Start () {
+		rb2D = gameObject.GetComponent<Rigidbody2D>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+	void FixedUpdate () {
+		float h = Input.GetAxis("Horizontal");
+		rb2D.AddForce ((Vector2.right * speed) * h);
+	}
+
+}
